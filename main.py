7ff4903119e17eb6c8159992814ab0b1571d200c
mjms <marina.sundiang@gmail.com>
2022-12-19 16:23:30 -0600

    initial commit. trying out APIs separately

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-#Knotion
+# Knotion
 # Ravelry + notion integration
 # first part: yarn database
 
-import requests, json
+import requests
+import json
 
 config_path = "config.json"
-with open(config_path,'r') as configfile:
+with open(config_path, 'r') as configfile:
     config = json.load(configfile)
 
 KNOTION_TOKEN = config['auth']['KNOTION_TOKEN']
-print(KNOTION_TOKEN)
-# DATABASE_ID = 
+DATABASE_ID = config['auth']['DATABASE_ID']
+RAVELRY_ID = config['auth']['RAVELRY_ID']
+headers = {
+    "Authorization": "Bearer " + KNOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+}
+###########
 
-# RAVELRY_ID = ''
+
+def readDatabase(DATABASE_ID, headers):
+    readURL = f'https://api.notion.com/v1/databases/{DATABASE_ID}'
+
+    res = requests.request("GET", readURL, headers=headers)
+    print(res.status_code)
+
+
+readDatabase(DATABASE_ID, headers)
